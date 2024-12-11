0f0488886af6fd2dcb1ef48f6c854a232728a9f8
Tim Pietrusky <timpietrusky@gmail.com>
2024-12-11 01:10:45 +0100

    fix: remove deleted component

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,22 +1,18 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { Button } from '@/components/ui/button';
-import NavBar from '@/components/wrapper/navbar';
+import { Button } from "@/components/ui/button";
 
-export default async function SuccessPage(
-  props: {
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-  }
-) {
-  return (
-    <main className="flex min-w-screen flex-col items-center justify-between">
-      <NavBar />
-      <h1 className="mt-[35vh] mb-3 scroll-m-20  text-5xl font-semibold tracking-tight transition-colors first:mt-0">
-        Welcome to OpenV
-      </h1>
-      <Link href="/generate" className='mt-4'>
-        <Button>Generate</Button>
-      </Link>
-    </main>
-  )
+export default async function SuccessPage(props: {
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+	return (
+		<main className="flex min-w-screen flex-col items-center justify-between">
+			<h1 className="mt-[35vh] mb-3 scroll-m-20  text-5xl font-semibold tracking-tight transition-colors first:mt-0">
+				Welcome to OpenV
+			</h1>
+			<Link href="/generate" className="mt-4">
+				<Button>Generate</Button>
+			</Link>
+		</main>
+	);
 }
