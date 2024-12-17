8f42df8330ca6bc98e7fe762ece89ddf841b64af
TimPietruskyRunPod <tim.pietrusky@runpod.io>
2024-12-17 23:45:31 +0100

    chore: remove console.log

--- utils/data/user/userCreate.ts
+++ utils/data/user/userCreate.ts
@@ -1,50 +1,48 @@
-"server only"
+"server only";
 
-import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
 import { userCreateProps } from "@/utils/types";
 
 export const userCreate = async ({
-  email,
-  first_name,
-  last_name,
-  profile_image_url,
-  user_id,
+	email,
+	first_name,
+	last_name,
+	profile_image_url,
+	user_id,
 }: userCreateProps) => {
-  const cookieStore = await cookies();
+	const cookieStore = await cookies();
 
-  const supabase = createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-      },
-    }
-  );
+	const supabase = createServerClient(
+		process.env.SUPABASE_URL!,
+		process.env.SUPABASE_SERVICE_KEY!,
+		{
+			cookies: {
+				get(name: string) {
+					return cookieStore.get(name)?.value;
+				},
+			},
+		}
+	);
 
-  try {
-    const { data, error } = await supabase
-      .from("user")
-      .insert([
-        {
-          email,
-          first_name,
-          last_name,
-          profile_image_url,
-          user_id,
-        },
-      ])
-      .select();
+	try {
+		const { data, error } = await supabase
+			.from("user")
+			.insert([
+				{
+					email,
+					first_name,
+					last_name,
+					profile_image_url,
+					user_id,
+				},
+			])
+			.select();
 
-    console.log("data", data);
-    console.log("error", error);
-
-    if (error?.code) return error;
-    return data;
-  } catch (error: any) {
-    throw new Error(error.message);
-  }
+		if (error?.code) return error;
+		return data;
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
 };
