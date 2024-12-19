43734a8d38ebe3e3178f4ef2b00ea96407e09c59
TimPietruskyRunPod <tim.pietrusky@runpod.io>
2024-12-19 16:51:40 +0100

    chore: remove unused files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
 	env: {
 		APP_ENV: process.env.APP_ENV || "development",
 	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "github.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "utfs.io",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
